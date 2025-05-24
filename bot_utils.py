d29c2aeabaeb971f36f8c337c3119208797b9ec6
Your Name <you@example.com>
2025-05-24 17:12:30 +0600

    Revert "added uv package manager"
    This reverts commit c443922c12d5847e7e62c0e8d9ca79e156b3bcdc.

--- bot_utils.py
+++ bot_utils.py
@@ -1,52 +1,58 @@
 import serial
 import time
+import random
 from PIL import ImageGrab
 import keyboard
 import pygetwindow as gw
 import config
 
-# Initialize the serial connection and pause execution
-# for 2 second to allow the serial connection to establish properly
+def init_serial():
+    """Initialize the serial connection and pause execution 
+    for 2 second to allow the serial connection to establish properly"""
+    ser = serial.Serial(config.COM_PORT, config.BAUD_RATE)
+    
+    time.sleep(2)
 
-#ser = serial.Serial(config.COM_PORT, config.BAUD_RATE)
-time.sleep(2)
-
-running = False
-
-
+is_running = True
 def toggle_running_state():
     """Toggle the running state of the program
     
     This function changes the value of the global variable 'is_running' to
-    its opposite. If 'running' currently True, it will be set to False,
-    and vice versa. It allows the program to start and stop based on user input."""
-    global running
-    running = not running
-    print(f'page down pressed, running = {running}')
+    its opposite. If 'is_running' currently True, it will be set to False,
+    and vice versa. It allows the programm to start and stop based on user input."""
+    global is_running
+    is_running = not is_running
 
+#bind the 'toggle_running_state' function to 'page down' key
+keyboard.add_hotkey('page down',toggle_running_state)
 
-def get_pixel_color(x, y):
+def actions_delay(a,b):
+    """Delay execution for a random time within the specified range.
+    Args:
+        a (float): The minimum delay time in seconds
+        b (float): The maximum delay time in seconds
+    """
+    random_delay = random.uniform(a, b)
+    time.sleep(random_delay) 
+
+def get_pixel_color(x,y):
     """Get the color of the pixel at the specified coordinates"""
     screen = ImageGrab.grab()
-    return screen.getpixel((x, y))
-
+    return screen.getpixel((x,y))
 
 def is_game_window_active():
     """Check if the target game windows is currently active"""
     active_window = gw.getActiveWindow()
     return active_window and config.TARGET_WINDOW_TITLE in active_window.title
 
-
 def is_alive_monster_in_target():
     """Check if monster is alive by comparing pixel color"""
     return get_pixel_color(*config.PIXEL_COORDINATES) == config.HP_PIXEL_COLOR
 
-
 def attack():
     """Send to serial port byte representation for specified characters """
-    ser.write(b'3')
-    ser.write(b'2')
-
+    ser.write(b'3') 
+    ser.write(b'2') 
 
 def get_next_target():
     """Send to serial port byte representation for specified characters """
--- main.py
+++ main.py
@@ -1,25 +1,15 @@
-from random import uniform
+import time
+from bot_utils import *
 
-import bot_utils
-from bot_utils import (is_game_window_active,
-                       is_alive_monster_in_target,
-                       get_next_target,
-                       attack, time)
+init_serial()
 
-
-def main():
-    while True:
-        if not bot_utils.running:
-            print('not running')
-            time.sleep(0.2)
-            continue
-        if is_game_window_active():
-            if is_alive_monster_in_target():
-                time.sleep(uniform(0.025, 0.036))
-                attack()
-            else:
-                get_next_target()
-                time.sleep(uniform(0.05, 0.1))
-
-if __name__ == '__main__':
-    main()
+while True:
+    if not is_running:
+        time.sleep(0.1)
+        continue 
+    if is_game_window_active():
+        if is_alive_monster_in_target():
+            actions_delay(0.025,0.036)
+            attack()
+        else:
+            get_next_target()
